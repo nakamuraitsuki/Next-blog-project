1545995a46f6bebaeab325f2c4983d55ab5dcbcf
nakamuraitsuki <isukin@gmail.com>
2024-12-13 14:49:29 +0900

    Layout適用

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
 import styles from "./page.module.css"
-import Header from "@/components/Header/Header"
-import Footer from "@/components/Footer/Footer"
+import Layout from "@/components/Layout/Layout"
 
 export default function Home() {
   return (
-    <div>
-      <Header/>
-      <main className={styles.content}>
-        <div className={styles.title}>
-          <h1>しゃべる葦原</h1>
-        </div>
-      </main>
-      <Footer/>
-    </div>
+    <Layout>
+      <div className={styles.content}>
+        <h1>HOME</h1>
+      </div>
+    </Layout>
   );
 }
