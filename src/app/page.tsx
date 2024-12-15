2f5bbe8e49816ca0d782892a79dca9aa7c6c8fbc
nakamuraitsuki <isukin@gmail.com>
2024-12-15 18:33:56 +0900

    セミコロン消しただけ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import styles from "./page.module.css"
 import Layout from "@/components/Layout/Layout"
-import { getAllPosts } from "@/lib/posts";
+import { getAllPosts } from "@/lib/posts"
 
 export default async function Home() {
   const posts = await getAllPosts();
