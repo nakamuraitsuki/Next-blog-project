dfd5c17e9f0f5b94ba3cb53a168318aefcf36691
nakamuraitsuki <isukin@gmail.com>
2024-12-15 22:41:26 +0900

    BlogCardListコンポーネント作成

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,7 @@
 import Layout from "../../components/Layout/Layout"
 import styles from "./blog.module.css"
 import { getAllPosts } from "@/lib/posts"
-import BlogCard from "@/components/BlogCard/BlogCard";
+import BlogCardList from "@/components/BlogCardList/BlogCardList";
 
 export default async function Blog() {
     const posts = await getAllPosts();
@@ -9,12 +9,8 @@ export default async function Blog() {
         <Layout>
             <h1>BLOG</h1>
             <h2>記事一覧</h2>
-            <div className={styles.cardList}>
-                {posts.map((post) => (
-                    <div key={post.slug}>
-                        <BlogCard post={post}/>
-                    </div>
-                ))}
+            <div className={styles.listConteiner}>
+                <BlogCardList posts={posts}/>
             </div>
         </Layout>
     );
