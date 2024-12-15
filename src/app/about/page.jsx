181595e8eaf1df23c8715efffbc94223e2d82e5a
nakamuraitsuki <isukin@gmail.com>
2024-12-16 00:53:36 +0900

    Aboutページ完成

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,26 +1,107 @@
 import styles from "./about.module.css"
 import Layout from "../../components/Layout/Layout"
 
+export function ProfileText() {
+    return(
+        <div className={styles.profile}>
+            <img 
+                src="/icon/apple_icon.jpg" 
+                alt="icon"
+                className={styles.icon}
+            />
+            <div>
+                <p className={styles.name}>nakamuraitsuki</p>
+                <p>埼玉大学工学部情報工学科の2年生</p>
+                <p>
+                    <a 
+                    href="https://maximum.vc/"
+                    className={styles.foreignLink}
+                    >
+                        プログラミングサークルMaximum
+                    </a>
+                    と
+                    <a
+                        href="http://mcs.xrea.jp/"
+                        className={styles.foreignLink}
+                    >
+                        埼玉大学推理小説研究会
+                    </a>
+                    に所属
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export function Profile() {
+    return(
+        <div>
+            <h2 className={styles.title}>
+                サイト作成者のプロフィール
+            </h2>
+            <ProfileText/>
+        </div>
+    )
+}
+
+export function Description() {
+    return(
+        <div>
+            <p>
+                プログラミングと読書が趣味です。
+            </p>
+            <p>
+                音楽を聴くのも好きで、通学中によく聴いています。たまにお絵かきもしたりします。
+            </p>
+            <p>
+                本の虫の一歩手前。好きな著者は太田紫織→西尾維新→米澤穂信→森博嗣→青崎有吾→白居智之…と変遷しています。
+            </p>
+            <p>
+                プログラミングは大学進学してから学び始め、現在精進中です。
+            </p>
+            <p>
+                ちょこちょこと調べながらWebサイトを構成してみたり、
+                C++を使って競技プログラミングをやってみたりしています。
+            </p>
+            <h3 className={styles.section}>ちょっと技術的なこと</h3>
+            <p>
+                触ったことのある言語としてはC・C++・Java・
+                JavaScript・TypeScript・GO等があります。
+            </p>
+            <p>個人開発でフロントエンドとバックエンドの両方を触った経験があります。</p>
+        </div>
+    )
+}
+
+export function AboutText() {
+    return(
+        <div>
+            <h2 className={styles.title}>このサイトの存在理由</h2>
+            <p>
+                自分自身が学んだことや
+                感情が動いたことを忘れないために、備忘録的な記事を書きたいと思い至り
+                このブログを作りました。
+            </p>
+            <p>
+                周りの人にはわざわざ言わないような事を、
+                深く掘った穴に叫ぶような気持ちで書いていこうと思っています。
+                サイトの名前『しゃべる葦原』はその思いが由来です。
+            </p>
+        </div>
+    )
+}
+
 export default function About() {
     return (
         <Layout>
-            <h1 className={styles.title}>About</h1>
-
-            <h2>サイト作成者のプロフィール</h2>
-            <p>おなまえ：nakamuraitsuki</p>
-            <p>かたがき：埼玉大学工学部情報工学科2年生</p>
-            <p>さーくる：プログラミングサークルMaximumと埼玉大学推理小説研究会に所属しています。推理小説研究会の方では一時期副会長をやっていました。</p>
-            <p>しゅみ：</p>
-            <p>読書　推し著者が太田紫織→西尾維新→米澤穂信→森博嗣→青崎有吾→白居智之のように変遷しています。</p>
-            <p>音楽聞くこと　最近はサブスクが台頭きているので、これをわざわざ趣味だとは言えないかもしれません。好みがジャンルを跨いでころころ変わります。</p>
-            <p>プログラミング　まだまだ初心者ですが、ちょこちょことしらべながらWebサイトを構成してみたり、C++を使って競技プログラミングをやってみたりしています。</p>
-            <h3>ちょっと技術的なこと</h3>
-            <p>触ったことのある言語としてはC・C++・Java・JavaScript・TypeScript・GOとかがあります。</p>
-            <p>フロントエンドとバックエンドどっちが得意？と聞かれると困ります。どちらも同じくらいです、悪い意味で。</p>
-            
-            <h2>このサイトの存在理由</h2>
-            <p>自分自身が学んだこと、感情の動きを忘れないために備忘録的な記事を書きたいと思い至り、このサイトを作りました。</p>
-            <p>周りの人にはわざわざ言わないような事を、深く掘った穴に叫ぶような気持ちで書いていこうと思っています。サイトの名前『しゃべる葦原』はその思いが由来です。</p>
+            <div className={styles.container}>
+                <h1 className={styles.hero}>About</h1>
+                <div className={styles.contents}>
+                    <Profile/>
+                    <Description/>
+                    <AboutText/>
+                </div>
+            </div>
         </Layout>
     );
 }
