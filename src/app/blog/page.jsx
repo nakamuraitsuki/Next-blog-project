3f56ad12916403392d012ad11fba0ce70ac4743e
nakamuraitsuki <isukin@gmail.com>
2024-12-15 18:10:34 +0900

    記事一覧ページに一旦一覧を表示

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,19 @@
-import Layout from "../../components/Layout/Layout";
+import Layout from "../../components/Layout/Layout"
+import { getAllPosts } from "@/lib/posts"
 
-export default function Blog() {
+export default async function Blog() {
+    const posts = await getAllPosts();
     return (
         <Layout>
             <h1>BLOG</h1>
+            <h2>記事一覧</h2>
+            {posts.map((post) => (
+                <div key={post.slug}>
+                    <p>{post.frontMatter.title}</p>
+                    <p>{post.frontMatter.description}</p>
+                    <p>{post.frontMatter.date}</p>
+                </div>
+            ))}
         </Layout>
     );
 }
