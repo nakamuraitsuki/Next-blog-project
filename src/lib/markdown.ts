b0b35c55c7a053a05a990d609723bf7f48ac520f
nakamuraitsuki <isukin@gmail.com>
2024-12-16 11:59:16 +0900

    表の読み取り

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -2,10 +2,12 @@ import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 import rehypeStringify from 'rehype-stringify'
+import remarkGfm from 'remark-gfm';
 
 export async function markdownToHTML(content: string) {
     const result = await unified()
         .use(remarkParse)
+        .use(remarkGfm)
         .use(remarkRehype)
         .use(rehypeStringify)
         .process(content);
