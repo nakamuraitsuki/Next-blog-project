73ec519d993319e8574cc52341a5cc729c231b77
nakamuraitsuki <isukin@gmail.com>
2024-12-24 18:02:16 +0900

    ページごとのメタデータ

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { notFound } from "next/navigation";
+import Head from 'next/head';
 import styles from "./slug.module.css"
 import Layout from "../../../components/Layout/Layout"
 import { getAllPosts, getPostBySlug } from "@/lib/posts"
@@ -29,6 +30,12 @@ export default async function Slug(props: PostProps) {
 
     return (
         <Layout>
+            <Head>
+                <title>{post.frontMatter.title || 'しゃべる葦原'}</title>
+                <meta name="description" content={post.frontMatter.description || ''} />
+                <meta property="og:title" content={post.frontMatter.title || 'しゃべる葦原'} />
+                <meta property="og:description" content={post.frontMatter.description || ''} />
+            </Head>
             <h1 className={styles.hero}>{post.frontMatter.title}</h1>
             <div className={styles.contents}>
                 <p className={styles.date}>{post.frontMatter.date}</p>
