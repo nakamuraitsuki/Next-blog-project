3531c7d92e6288061800a9a91476a4a5f0ebb61a
nakamuraitsuki <isukin@gmail.com>
2024-12-31 22:14:31 +0900

    フォーマット整えた

--- src/lib/markdown/markdown.ts
+++ src/lib/markdown/markdown.ts
@@ -1,13 +1,13 @@
-import { unified } from 'unified'
-import remarkParse from 'remark-parse'
-import remarkRehype from 'remark-rehype'
-import rehypeStringify from 'rehype-stringify'
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import rehypeStringify from 'rehype-stringify';
 import remarkGfm from 'remark-gfm';
 import rehypeHighlight from 'rehype-highlight';
 import rehypeSlug from 'rehype-slug';
 import { Nodes } from "mdast";
-import { Handler } from "mdast-util-to-hast"
-import { tocPlugin, tweetPlugin } from "./plugins"
+import { Handler } from "mdast-util-to-hast";
+import { tocPlugin, tweetPlugin } from "./plugins";
 import { TableOfContentsItem } from '../type';
 import { tweetHandler } from './utils';
 
@@ -16,9 +16,9 @@ interface MarkdownContent {
     html: string;
 }
 
-//remarkRehypeに渡す拡張ハンドラ
+//remarkRehypeに渡すカスタムハンドラ
 const handlers: Partial<Record<Nodes['type'], Handler>> = {
-    tweet: tweetHandler
+    tweet: tweetHandler//tweetNodeに対する処理を定義
 };
 
 export async function markdownToHTML(content: string): Promise<MarkdownContent> {
