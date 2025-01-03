0cd1704535815dd56dac56fe9361bc9dffad9e8d
nakamuraitsuki <isukin@gmail.com>
2025-01-03 21:20:55 +0900

    型拡張

--- src/lib/type/nodes.ts
+++ src/lib/type/nodes.ts
@@ -1,8 +1,9 @@
-import { TweetNode, LinkCardNode } from "../type"
+import { TweetNode, LinkCardNode, CodeHeaderNode } from "../type"
 
 declare module 'mdast' {
     interface RootContentMap {
         tweet: TweetNode;//TweetNodeを許容するよう設定
         linkCard: LinkCardNode;//LinkCardNodeを許容するよう設定
+        codeHeader: CodeHeaderNode;//CodeHeaderNodeを許容するよう設定
     }
 }
