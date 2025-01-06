eb5ae17f45e055400167c3f03e13155af2ab7727
nakamuraitsuki <isukin@gmail.com>
2025-01-06 23:37:56 +0900

    パンくずリストについて記述

--- src/components/Tweet/Tweet.tsx
+++ src/components/Tweet/Tweet.tsx
@@ -1,6 +1,7 @@
 import styles from "./Tweet.module.css";
+import { ReactNode } from "react";
 
-export const Tweet = ({ children }) => {
+export const Tweet = ({ children }: {children: ReactNode} ) => {
     return(
         <div className={styles.wrap}>
             <div className={styles.tweet}>
