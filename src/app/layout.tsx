29df596659224ccf6b4a36274b24197d07a76105
nakamuraitsuki <ituki1911n@gmail.com>
2025-04-15 00:26:46 +0900

    feat: analytics追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -39,6 +40,7 @@ export default function RootLayout({
     <html lang="ja">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <Analytics/>
       </body>
     </html>
   );
